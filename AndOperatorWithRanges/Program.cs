220a1b0da6e4c0de25d46d4cc0a96f3f866c759a
yavuz06 <yavuzselmkaya@gmail.com>
2023-04-04 13:58:03 +0300

    week5

---
+++ AndOperatorWithRanges/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Enter the current temperature:");
+int currenttemp = int.Parse(Console.ReadLine());
+
+if(currenttemp <= 0)
+{
+    Console.WriteLine("Freezing");
+}
+else if (currenttemp > 0 && currenttemp <= 5 )
+{
+    Console.WriteLine("Cold");
+}
+else if (currenttemp > 5 && currenttemp <= 10 )
+{
+    Console.WriteLine("Chilly");
+}
+else if (currenttemp > 10 &&currenttemp <= 15 )
+{
+    Console.WriteLine("Cool");
+}
+else if (currenttemp > 15 &&currenttemp <= 30 )
+{
+    Console.WriteLine("Warm");
+}
+else
+{
+    Console.WriteLine("Boilin hot");
+}
---
+++ Week5AndOrLogicalOperators/Program.cs
@@ -0,0 +1,27 @@
+﻿//and(&&) or(||) logical operations
+
+string username, password;
+Console.WriteLine("Enter your username:");
+username = Console.ReadLine();
+
+Console.WriteLine("Enter your password");
+password = Console.ReadLine();
+
+//if(username == "admin" && password == "Yavuz1910")
+//{
+//    Console.WriteLine("Welcome");
+    
+//}
+//else
+//{
+//    Console.WriteLine("Invalid username or password");
+//}
+
+if(username != "admin" || password != "admin1910")
+    {
+    Console.WriteLine("Invalid username or password");
+}
+else
+{
+    Console.WriteLine("Welcome");
+}
